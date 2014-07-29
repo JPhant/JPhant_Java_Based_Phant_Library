1853931a66eac58dfcba6096549d65a2dc559999
JPhant <JPhant@reallymymail.com>
2014-07-28 21:59:33 -0400

    Update JPhant_Config_0.java

--- JPhant_Config_0.java
+++ JPhant_Config_0.java
@@ -14,12 +14,17 @@ public class JPhant_Config_0 implements JPhantConfig {
 	
 	// *** If running your own Phant Server, enter URL below *** //
 	private static final String sPHANT_BASE_URL = "https://data.sparkfun.com/";
-	
+
+	// *** If you're behind a Firewall/Proxy Server, enter ProxyHost:ProxyPort below, otherwise leave blank *** //
+	// *** Example: sPROXYHOST_AND_PORT = "CompanyProxyServer:8080"; *** //
+	private static final String sPROXYHOST_AND_PORT = "";
+
 	// *** getMethod can return GET or POST (POST is recommended so PrivateKey & Data are encrypted in transit) *** //
 	public Method getMethod() { return Method.POST; }
 
 	// *** *** *** *** Don't touch anything below this Line *** *** *** //
 	public String getBaseURL() { return sPHANT_BASE_URL; }
+	public String getProxyHostAndPort() { return sPROXYHOST_AND_PORT; }
 	public String getPublicKey() { return sPHANT_PUBLIC_KEY; }
 	public String getPrivateKey() { return sPHANT_PRIVATE_KEY; }
 	//public String getDeleteKey() { return sPHANT_DELETE_KEY; }
